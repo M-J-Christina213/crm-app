db7f6b93b1db7f1b4d38eab098db77366123ba41
M-J-Christina213 <m.j.christina213@gmail.com>
2026-05-08 11:55:20 +0530

    add route for Leads component in main application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import App from "./App";
 import Dashboard from "./pages/Dashboard";
+import Leads from "./pages/Leads";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/app" element={<Dashboard />} />
+      <Route path="/leads" element={<Leads />} />
     </Routes>
   </BrowserRouter>
 );
